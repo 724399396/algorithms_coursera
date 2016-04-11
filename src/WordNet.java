1e635eba280c94c9d531587d0ec674bc8520d290
liww.li <liww.li@qunar.com>
2016-04-11 18:11:27 +0800

    update WordNet

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,22 +1,26 @@
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.ST;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class WordNet {
     private Digraph digraph;
-    private ST<Integer, List<String>> map;
+    private Map<Integer, List<String>> id2Wrods;
+    private Map<String, Integer> word2ID;
     private Set<String> nouns;
+    private SAP sap;
 
     public WordNet(String synsets, String hypernyms) {
         checkArgNull(synsets, hypernyms);
         In in = new In(synsets);
-        map = new ST();
+        id2Wrods = new HashMap<>();
         nouns = new HashSet<>();
+        word2ID = new HashMap<>();
         while (in.hasNextLine()) {
             String line = in.readLine();
             String[] splits = line.split(",");
@@ -26,19 +30,21 @@ public class WordNet {
             for (String word : words) {
                 wordsList.add(word);
                 nouns.add(word);
+                word2ID.put(word, id);
             }
             String gloss = splits[2];
-            map.put(id, wordsList);
+            id2Wrods.put(id, wordsList);
         }
         In in2 = new In(hypernyms);
         String[] lines = in2.readAllLines();
-        digraph = new Digraph(map.size());
+        digraph = new Digraph(id2Wrods.size());
         for (String line : lines) {
             String[] ids = line.split(",");
             for (int i = 1; i < ids.length; i++) {
                 digraph.addEdge(Integer.parseInt(ids[0]), Integer.parseInt(ids[i]));
             }
         }
+        sap = new SAP(digraph);
     }
 
     public Iterable<String> nouns() {
@@ -52,12 +58,12 @@ public class WordNet {
 
     public int distance(String nounA, String nounB) {
         checkArgNull(nounA, nounB);
-        return 0;
+        return sap.length(word2ID.get(nounA), word2ID.get(nounB));
     }
 
     public String sap(String nounA, String nounB) {
         checkArgNull(nounA, nounB);
-        return "";
+        return id2Wrods.get(sap.ancestor(word2ID.get(nounA), word2ID.get(nounB))).toString();
     }
 
     private void checkArgNull(Object... input) {
