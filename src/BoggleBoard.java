3ca6e2d8f88cab9b385a243c08cc04dbd557bde2
liww.li <liww.li@qunar.com>
2016-04-27 19:09:35 +0800

    boggle init

---
+++ src/BoggleBoard.java
@@ -0,0 +1,104 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class BoggleBoard {
+    private int rows;
+    private int cols;
+    private char[][] board;
+
+    // Initializes a random 4-by-4 Boggle board.
+    // (by rolling the Hasbro dice)
+    public BoggleBoard() {
+        rows = 4;
+        cols = 4;
+        generateTable();
+    }
+
+    // Initialize a random M-by-N Boggle board.
+    // (using the frequency of letters in the English language
+    public  BoggleBoard(int M, int N) {
+        rows = M;
+        cols = N;
+        generateTable();
+    }
+
+    // Initializes a Boggle board from the specified filename.
+    public BoggleBoard(String filename) {
+        In in = new In(filename);
+        rows = in.readInt();
+        cols = in.readInt();
+        board = new char[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                board[i][j] = in.readString().toCharArray()[0];
+            }
+        }
+    }
+
+    // Initializes a Boggle board from the 2d char array.
+    // (with 'Q' representing the two-letter sequence "Qu")
+    public BoggleBoard(char[][] a) {
+        rows = a.length;
+        cols = a[0].length;
+        board = a;
+    }
+
+    // Returns the number of rows.
+    public int rows() {
+        return rows;
+    }
+
+    // Returns the number of columns.
+    public int cols() {
+        return cols;
+    }
+
+    // Returns the letter in tow i and column j.
+    // (With 'Q' representing the two-letter Sequence "Qu")
+    public char getLetter(int i, int j) {
+        return board[i][j];
+    }
+
+    // Returns a String representation of the board
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+               sb.append(board[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    private char randomChar() {
+        return (char) ('A' + StdRandom.uniform(0, 26));
+    }
+
+    private void generateTable() {
+        board = new char[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                board[i][j] = randomChar();
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        BoggleBoard board = new BoggleBoard(args[1]);
+        StdOut.println(board);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board))
+        {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
+    }
+
+}
